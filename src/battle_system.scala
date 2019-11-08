9ad3ceccc15bf3d85c4e391d181108958df42e47
luo <wupinglu@buffalo.edu>
2019-11-07 20:50:23 -0500

    11

--- src/battle_system.scala
+++ src/battle_system.scala
@@ -1,3 +1,13 @@
-class battle_system {
+import akka.actor._
 
+case class AddParty()
+case class RemoveParty()
+case class BattleStarted()
+case object Update
+case class Turn()
+
+class battle_system extends Actor{
+  def receive:Receive={
+    case Update =>
+  }
 }
